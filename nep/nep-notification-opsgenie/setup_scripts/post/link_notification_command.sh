52d24db21e821f73970dab781da1248df8d7a776
Rocco Pezzani <Rocco.Pezzani@wuerth-it.com>
2026-07-31 18:48:48 +0200

    fix: NEP-925 Fix other minor scripts

--- nep/nep-notification-opsgenie/setup_scripts/post/link_notification_command.sh
+++ nep/nep-notification-opsgenie/setup_scripts/post/link_notification_command.sh
@@ -42,8 +42,8 @@ if [[ $neteye_deployment == 'single_node' ]]; then
 fi
 if [[ $neteye_deployment == 'cluster' ]]; then
     if [[ $neteye_node_type == 'node' ]]; then
-        SERVICE="icinga2-master"
-        if systemctl is-active "$SERVICE" > /dev/null ; then
+        DRBD_MOUNTPOINT="icinga2"
+        if is_drbd_mounted "$DRBD_MOUNTPOINT" ; then
             link_notification_command
         else
             echo "[i] Inactive Cluster Node. Skipping."
